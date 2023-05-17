21b9cc4ab522543eb5c55f106250ecfec6c46e8a
hassaan adil <109539175+MHassaanAdil440@users.noreply.github.com>
2023-05-17 05:30:03 +0500

    playing with files

--- api2.js
+++ api2.js
@@ -1,14 +1,14 @@
-var http = require('http');
+var http = require("http");
 // var data = require('./data2');
 
 // console.log(data);
 const name1 = process.argv[2];
 const age1 = process.argv[3];
-const data =[
-    {name: name1, age:age1},
-];
-var server = http.createServer(function(req,res){
-    res.writeHead(201,{'Content-Type':'application/json'});
+const data = [{ name: name1, age: age1 }];
+var server = http
+  .createServer(function (req, res) {
+    res.writeHead(201, { "Content-Type": "application/json" });
     res.write(JSON.stringify(data));
     res.end();
-}).listen(8091)
+  })
+  .listen(8091);
---
+++ fs.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+
+const path1 = path.join(__dirname, 'files');
+// console.log(path1)
+if(!fs.existsSync(path1)){
+    fs.mkdirSync(path1);}
+for(i=0; i<4; i++){
+    fs.writeFileSync(path1+"/hello"+i+".txt", 'Hello World')
+}
+
+// const fs = require('fs');
+// const path = require('path');
+
+// const folderPath = path.join(__dirname, 'files');
+
+// // Create the "files" folder if it doesn't exist
+// if (!fs.existsSync(folderPath)) {
+//   fs.mkdirSync(folderPath);
+// }
+
+// // Loop to create four text files
+// for (let i = 1; i <= 4; i++) {
+//   const fileName = `file${i}.txt`;
+//   const filePath = path.join(folderPath, fileName);
+//   const fileContent = 'Hello, world!';
+
+//   fs.writeFileSync(filePath, fileContent);
+
+//   console.log(`Created ${fileName}`);
+// }
